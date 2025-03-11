3caf79bf82e1bd87f777510449aa2b975d6f81af
renoyurii <renoyuri@gmail.com>
2025-03-10 23:44:51 -0300

    - Adicionado TestePilha.java no ProjetoPilha1 - Adicionado TestePilhaEncadeada.java no ProjetoPilha2 - Adicionado PilhaJavaUtil.java no ProjetoPilha3

---
+++ ExerciciosJava/ProjetoPilha3/PilhaJavaUtil.java
@@ -0,0 +1,16 @@
+package ExerciciosJava.ProjetoPilha3;
+
+import java.util.Stack;
+
+public class PilhaJavaUtil {
+    public static void main(String[] args) {
+        Stack<Integer> pilha = new Stack<>();
+        pilha.push(100);
+        pilha.push(200);
+        pilha.push(300);
+        System.out.println("Topo da pilha: " + pilha.peek());
+        System.out.println("Removendo: " + pilha.pop());
+        System.out.println("Pilha atual: " + pilha);
+    }
+
+}
